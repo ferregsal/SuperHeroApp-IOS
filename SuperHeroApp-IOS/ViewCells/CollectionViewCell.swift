4f4ff99927a1b4da6a2e9280afd833ed3aa39f25
fregidorsalazar <fregidorsalazar@gmail.com>
2024-09-03 14:28:44 +0200

    creada vista y celda

---
+++ SuperHeroApp-IOS/ViewCells/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  SuperHeroApp-IOS
+//
+//  Created by Mañanas on 3/9/24.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var superheroImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
